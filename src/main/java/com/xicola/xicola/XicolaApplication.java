805139f4a3219c2b50d2e793e7de7fff236f3d20
Meldo Maunze <mlmaunze@gmail.com>
2024-06-28 11:55:48 +0200

    first commit

---
+++ src/main/java/com/xicola/xicola/XicolaApplication.java
@@ -0,0 +1,13 @@
+package com.xicola.xicola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XicolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XicolaApplication.class, args);
+	}
+
+}
