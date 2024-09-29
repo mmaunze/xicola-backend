868b7a57867d4f240c982444aaf6731f61378c19
mmaunze <mlmaunze@gmail.com>
2024-09-29 17:50:18 +0200

    added autenticacao por roles

--- src/main/java/mz/co/mefemasys/xicola/backend/security/WebSecurityConfig.java
+++ src/main/java/mz/co/mefemasys/xicola/backend/security/WebSecurityConfig.java
@@ -5,12 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,10 +20,8 @@ import mz.co.mefemasys.xicola.backend.security.services.UtilizadorDetailsService
 
 @Configuration
 @EnableMethodSecurity
-// (securedEnabled = true,
-// jsr250Enabled = true,
-// prePostEnabled = true) // by default
-public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
+
   @Autowired
   UtilizadorDetailsServiceImpl userDetailsService;
 
@@ -38,27 +33,14 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
     return new AuthTokenFilter();
   }
 
-//  @Override
-//  public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-//    authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-//  }
-  
   @Bean
   public DaoAuthenticationProvider authenticationProvider() {
       DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-       
       authProvider.setUserDetailsService(userDetailsService);
       authProvider.setPasswordEncoder(passwordEncoder());
-   
       return authProvider;
   }
 
-//  @Bean
-//  @Override
-//  public AuthenticationManager authenticationManagerBean() throws Exception {
-//    return super.authenticationManagerBean();
-//  }
-  
   @Bean
   public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
     return authConfig.getAuthenticationManager();
@@ -69,33 +51,51 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
     return new BCryptPasswordEncoder();
   }
 
-//  @Override
-//  protected void configure(HttpSecurity http) throws Exception {
-//    http.cors().and().csrf().disable()
-//      .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-//      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-//      .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-//      .antMatchers("/api/test/**").permitAll()
-//      .anyRequest().authenticated();
-//
-//    http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-//  }
-  
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+    // Definindo rotas públicas
+    String[] ROTAS_PUBLICAS = {
+        "/api/auth/**", 
+        "/", 
+        "/api/test/**"
+    };
+
+    // Definindo rotas privadas (apenas requerem autenticação)
+    String[] ROTAS_PRIVADAS = {
+        "/geral/**", 
+  
+    };
+
+    // Definindo rotas para roles específicas
+    String[] ROTAS_ADMIN = {
+        "/api/admin/**"
+    };
+
+    String[] ROTAS_PROFESSOR = {
+        "/api/professor/**"
+    };
+
+    String[] ROTAS_ALUNO = {
+        "/api/aluno/**"
+    };
+
     http.csrf(csrf -> csrf.disable())
         .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> 
-          auth.requestMatchers("/api/auth/**","/").permitAll()
-              .requestMatchers("/api/test/**").permitAll()
-              .anyRequest().authenticated()
+          auth.requestMatchers(ROTAS_PUBLICAS).permitAll()  // Rotas públicas
+              .requestMatchers(ROTAS_PRIVADAS).authenticated() // Rotas privadas (apenas autenticadas)
+              .requestMatchers(ROTAS_ADMIN).hasRole("ADMIN")  // Rotas de Admin
+              .requestMatchers(ROTAS_PROFESSOR).hasRole("PROFESSOR") // Rotas de Professor
+              .requestMatchers(ROTAS_ALUNO).hasRole("ALUNO")  // Rotas de Aluno
+              .anyRequest().authenticated() // Qualquer outra requisição precisa de autenticação
         );
-    
+
     http.authenticationProvider(authenticationProvider());
 
     http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-    
+
     return http.build();
   }
 }
